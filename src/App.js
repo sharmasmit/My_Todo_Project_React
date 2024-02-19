ac2cc4ca4fec2c3afb36706a163431cf18538a67
sharmasmit <smeet.sharma007@gmail.com>
2024-02-19 18:48:30 +0530

    Icons added

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquarePlus } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 function App() {
-
-  const generateBtn = document.querySelector('.generate');
   const inputText = document.querySelector('.add-input');
-  const todoList = document.querySelector('.todo-List');
+  const todoList = document.querySelector('.todo-list');
   const filterOptions = document.querySelector('.filter-todo');
 
-  generateBtn.addEventListener("click", addTodo);
-  todoList.addEventListener("click", deleteCheck);
-  filterOptions.addEventListener("click", filterTodo);
+  function addTodo(e) {
+    e.preventDefault();
 
-  function addTodo(event) {
-    event.preventDefault();
-
-    const todoDiv = document.createElement('div');
+    const todoDiv = document.createElement("div");
     todoDiv.classList.add('todo');
 
-    const newTodo = document.createElement('li');
+    const newTodo = document.createElement("li");
     newTodo.classList.add('todo-item');
     newTodo.innerText = inputText.value;
     todoDiv.appendChild(newTodo);
 
-    const checkBtn = document.createElement('button');
+    const checkBtn = document.createElement("button");
     checkBtn.classList.add('complete-btn');
-    checkBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    checkBtn.innerHTML = '<FontAwesomeIcon icon={faCheck} />';
     todoDiv.appendChild(checkBtn);
 
-    const trashBtn = document.createElement('button');
+    const trashBtn = document.createElement("button");
     trashBtn.classList.add('trash-btn');
-    trashBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    trashBtn.innerHTML = '<FontAwesomeIcon icon={faTrash} />';
     todoDiv.appendChild(trashBtn);
 
     todoList.appendChild(todoDiv);
 
-    inputText.value = ' ';
+    inputText.value = '';
   }
 
-  function deleteCheck(e) {
+  todoList.addEventListener("click", (e) => {
     const item = e.target;
     if (item.classList[0] === 'trash-btn') {
       const todo = item.parentElement;
@@ -50,16 +47,15 @@ function App() {
       const todo = item.parentElement;
       todo.classList.toggle("completed");
     }
-  }
+  });
 
-  function filterTodo(e) {
-    const todo = todoList.childNodes;
-    todo.forEach(function (todo) {
+  filterOptions.addEventListener("click", (e) => {
+    const todos = todoList.childNodes;
+    todos.forEach(function (todo) {
       switch (e.target.value) {
         case "all":
           todo.style.display = 'flex';
           break;
-
         case "completed":
           if (todo.classList.contains('completed')) {
             todo.style.display = "flex";
@@ -67,7 +63,6 @@ function App() {
             todo.style.display = "none";
           }
           break;
-
         case "uncompleted":
           if (!todo.classList.contains("completed")) {
             todo.style.display = "flex";
@@ -75,26 +70,27 @@ function App() {
             todo.style.display = "none";
           }
           break;
+        default:
+          break;
       }
     });
-  }
+  });
 
   return (
     <div className="App">
       <div className="main">
-        <h1 className="hrad">Smit's Todo Project</h1>
+        <h1 className="head">Smit's Todo Project</h1>
         <div className="input-items">
-          <input type="text" className="add-input" placeholder="Add Some Text"/>
-            <button type="submit" className="generate"><i class="fa-solid fa-square-plus"></i></button>
+          <input type="text" className="add-input" placeholder="Add Some Text" />
+          <button type="submit" className="generate" onClick={addTodo}><FontAwesomeIcon icon={faSquarePlus} /></button>
 
-            <div className="select">
-              <select name="todos" className="filter-todo">
-                <option value="all">All</option>
-                <option value="completed">Completed</option>
-                <option value="uncompleted">Uncompleted</option>
-              </select>
-            </div>
-
+          <div className="select">
+            <select name="todos" className="filter-todo">
+              <option value="all">All</option>
+              <option value="completed">Completed</option>
+              <option value="uncompleted">Uncompleted</option>
+            </select>
+          </div>
         </div>
 
         <div className="todo-container">
